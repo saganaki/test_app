5f4deba1e1045f962d4810025ad2ed1d967fa090
D Malaxos <davidx@mindspring.com>
2011-06-04 13:46:42 -0400

    Init commit yit agit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'b01e8d71c12b6f0133cf1987979cb370a5fb1dc3396338ae26b758b7e4fe01cd6bcfa754d2c21a513456827f6b47fdcdbede80ca27860117ce6cac3a9b62acbe'
